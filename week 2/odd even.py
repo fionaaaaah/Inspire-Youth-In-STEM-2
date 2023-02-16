95d9b5e496909210f40a633775723e19a039fa37
fionaaaaah <muriithinfiona@gmail.com>
2023-02-16 00:46:20 -0800

    mean.py

--- week 2/odd even.py
+++ week 2/odd even.py
@@ -1,5 +1,35 @@
+'''
 number = 0
 if (number%2 == 0):
     print("even number")
 else:
     print("odd number")
+'''
+'''
+num = 0
+if (num%2) == 0:
+    print("even number")
+else:
+    print("odd number")
+'''
+'''
+print("***The values below are odd numbers***")
+for odd_numbers in range(1,101):
+    if(odd_numbers%2 !=0):
+        print(odd_numbers)
+
+print("***The values below are even numbers***")
+for even_numbers in range(0,102):
+    if(even_numbers%2 ==0):
+        print(even_numbers)
+'''
+ 
+S = 1 
+E = 100
+for num in range(S, E+1):
+    if num>1:
+        for i in range(2,num):
+            if(num % i==0):
+                break
+            else:
+                print(num, end=" ")
