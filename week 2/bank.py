447041ac65a168f719a88a02809845f2c7c9047c
fionaaaaah <muriithinfiona@gmail.com>
2023-02-18 15:48:01 -0800

    bank.py

--- week 2/bank.py
+++ week 2/bank.py
@@ -13,4 +13,28 @@
 #5% if income is less than 100k
 
 #print gross income,net income
-income = input("Enter your net income")
+
+net_income = int(input("Enter your Net income "))
+
+#above 300k
+if(net_income >= 300000):
+	gross_income = net_income + ((30/100)*net_income)
+	print("Your Net income is {} hence your Gross income is {}".format(net_income, int(gross_income)))
+
+#150k to less than 300k
+elif(net_income >= 150000) & (net_income < 300000):
+	gross_income = net_income + ((19/100)*net_income)
+	print("Your Net income is {} hence your Gross income is {}".format(net_income, int(gross_income)))
+
+#100k to less than 150k
+elif(net_income >= 100000) & (net_income < 150000):
+	gross_income = net_income + ((16/100)*net_income)
+	print("Your Net income is {} hence your Gross income is {}".format(net_income, int(gross_income)))
+
+#from 1 to less than 100k
+elif(net_income >= 1) & (net_income < 100000):
+	gross_income = net_income + ((5/100)*net_income)
+	print("Your Net income is {} hence your Gross income is {}".format(net_income, int(gross_income)))
+
+else:
+	print("Invalid argument!")
