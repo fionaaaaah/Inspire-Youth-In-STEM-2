3f1907580aa1bcdf868e86b4e8550836fdc92836
fionaaaaah <muriithinfiona@gmail.com>
2023-02-18 04:34:10 -0800

    operators.py

---
+++ week 2/operators.py
@@ -0,0 +1,44 @@
+#!user/bin/env python3
+x = 4
+y = 7
+
+#This is a single line comment
+#python program to illustrate 
+#Name: fiona
+#Email: muriithinfiona@gmail.com
+#Date : 17th Feb 2023
+#File : operators.py
+
+#Addition
+sum_nums = x + y
+#subtraction
+dif_nums = x - y
+#multiplication
+pro_nums = x * y
+#division
+quo_nums = x / y
+#modulus
+
+
+
+print("sum of the numbers:",sum_nums)
+print("dif of the numbers:",dif_nums)
+print("pro of the numbers:",pro_nums)
+print("quo of the numbers:",quo_nums)
+
+
+
+#left shift represents division
+num1 = 64 << 2
+#right shift represents mutiplication
+num2 = 64 >> 2
+print(num1)
+print(num2)
+
+#ADD &
+
+#QR ||
+
+#XOR
+
+#NOT
