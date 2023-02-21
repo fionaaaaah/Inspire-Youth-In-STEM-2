5e13c882f1958b6987af4f9c1124391bd28c1c6c
fionaaaaah <muriithinfiona@gmail.com>
2023-02-21 11:15:37 -0800

    loops,py

---
+++ week 3/assignment_2.py
@@ -0,0 +1,3 @@
+#Write a program that solves quadratic expression
+
+
---
+++ week 3/loops.py
@@ -0,0 +1,19 @@
+#do while
+#while(True):
+    #print("My name is Fiona")
+'''
+num = 24
+while(True):
+    if(num > 20):
+        print(num)
+'''
+
+age = 24
+gender = "male"
+if(age< 25 & gender = "male"):
+    if(weight > 75 | height = 125):
+        print("You are obese")
+    elif:
+        print("You are healthy")
+elif:
+    print("You are just funny")
