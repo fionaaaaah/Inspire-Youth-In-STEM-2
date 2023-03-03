610d39466503c88ccc6f89af0999dff5a03cff1c
fionaaaaah <muriithinfiona@gmail.com>
2023-03-03 11:17:14 +0300

    home.html

--- week 4/files.py
+++ week 4/files.py
@@ -1,3 +1,8 @@
-file_name = open("C:\\Users\\ADMIN\\Documents\\Inspire Youth In STEM\\week 4\\test.txt.",'r')
+file_name = open("C:\\Users\\ADMIN\\Documents\\Inspire Youth In STEM\\week 4\\test.txt",'r')
 
 print(file_name.read())
+
+
+#file_name = open("C:\\Users\\ADMIN\\Documents\\Inspire-Youth-in-STEM\\Week-4\\test.txt",'r')
+
+#print(file_name.read())
---
+++ week 4/object_oriented_programming.py
@@ -0,0 +1,6 @@
+#Class - a group of objects
+#objects - instance of the class with unique parameters
+
+# Students - Mark mercy Leon
+#student - Mercy : age,weight,height,residence
+#Cars - volvo toyota mercedes
